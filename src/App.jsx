094f8b974876d68ccf4da16b5b080e1b708b0c88
shubham-sanvariya <shubhamsanvariyait.com>
2023-05-15 18:25:23 +0530

    implementing Canvas and components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
