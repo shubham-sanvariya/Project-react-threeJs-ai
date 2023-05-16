a2a516f516218441e1f9c8e2fca19b4fa1c68bb7
shubham-sanvariya <shubhamsanvariyait.com>
2023-05-17 00:23:54 +0530

    adding CustomButton on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
 
 import state from '../store';
+import { CustomButton } from "../components";
 import {headContainerAnimation,
         headContentAnimation,
         headTextAnimation,
@@ -40,7 +41,10 @@ const Home = () => {
                 </p>
 
                 <CustomButton 
-                
+                  type="filled"
+                  title="Customize It"
+                  handleClick={() => state.intro = false}
+                  customStyles="w-fit px-4 py-2.5 font-bold text-sm"
                 />
               </motion.div>
           </motion.div>
