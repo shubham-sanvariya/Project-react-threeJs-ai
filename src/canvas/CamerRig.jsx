91ef7d3377ec8febfd8deaa7964f72b68368e089
shubham-sanvariya <shubhamsanvariyait.com>
2023-05-18 19:39:28 +0530

    setting CameraRig target position& model rotation

--- src/canvas/CamerRig.jsx
+++ src/canvas/CamerRig.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-const CamerRig = () => {
-  return (
-    <div>CamerRig</div>
-  )
+import Shirt from './Shirt'
+import { useSnapshot } from 'valtio';
+import state from '../store';
+import { easing } from 'maath';
+import { useFrame } from '@react-three/fiber';
+
+const CamerRig = ({ children }) => {
+
+  const group = useRef();
+  const snap = useSnapshot(state);
+
+  // this hook allows you to execute code on every render frame
+  useFrame((state,delta) => {
+    const isBreakPoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    // set the initial position of the model
+    let targetPosition = [-0.5, 0, 2];
+    if(snap.intro) {
+      if (isBreakPoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
+    } else {
+      if(isMobile) targetPosition = [0, 0, 2.5]
+      else targetPosition = [0, 0, 2];
+    }
+
+    // set the model rotation smoothly
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    )
+  })
+
+
+
+  return <group ref={group}>{children}</group>
+  
 }
 
 export default CamerRig
