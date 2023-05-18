c1be109c7cdef6fa9e7e434ac9af21846d8128ab
shubham-sanvariya <shubhamsanvariyait.com>
2023-05-18 18:58:34 +0530

    adding react three properties to show shirt

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -3,11 +3,23 @@ import React from 'react'
 import Shirt from './Shirt'
 import Backdrop from './Backdrop'
 import CamerRig from './CamerRig'
+import { Canvas } from '@react-three/fiber'
+import { Center, Environment } from '@react-three/drei'
 
 const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
+      {/* <CamerRig> */}
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      {/* </CamerRig> */}
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
