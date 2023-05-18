bf8dbc37503d3181fcffc67ccf547a15d7d38b15
shubham-sanvariya <shubhamsanvariyait.com>
2023-05-18 18:39:06 +0530

    setting shirt geometry materail

--- src/canvas/Shirt.jsx
+++ src/canvas/Shirt.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useSnapshot } from 'valtio'
+import state from '../store'
+import { useGLTF, useTexture } from '@react-three/drei';
 
 const Shirt = () => {
+
+    const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+
+    const logoTexture = useTexture(snap.logoDecal);
+    const fullTexture = useTexture(snap.fullDecal);
+    
   return (
-    <div>Shirt</div>
+    <group>
+        <mesh
+            castShadow
+            geometry={nodes.T_Shirt_male.geometry}
+            material={materials.lambert1} 
+            // lambert1 is type of material
+            material-roughness={1}
+            dispose={null}
+        >
+
+        </mesh>
+    </group>
   )
 }
 
