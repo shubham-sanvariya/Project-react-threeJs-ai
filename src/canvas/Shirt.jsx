0695bc19b4dac15146871fda3d43259a72ba9985
shubham-sanvariya <shubhamsanvariyait.com>
2023-05-25 23:22:56 +0530

    applying full texture & logoTexture

--- src/canvas/Shirt.jsx
+++ src/canvas/Shirt.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSnapshot } from 'valtio'
 import state from '../store'
-import { useGLTF, useTexture } from '@react-three/drei';
+import { Decal, useGLTF, useTexture } from '@react-three/drei';
 
 const Shirt = () => {
 
@@ -21,7 +21,22 @@ const Shirt = () => {
             material-roughness={1}
             dispose={null}
         >
-
+          {snap.isFullTexture && (
+            <Decal
+              position={[0, 0, 0]}
+              rotation={[0, 0, 0]}
+              scale={1}
+              map={fullTexture}
+            />
+          )}
+          {snap.isLogoTexture && (
+            <Decal
+              position={[0, 0.04, 0.15]}
+              rotation={[0, 0, 0]}
+              scale={0.15}
+              map={logoTexture}
+            />
+          )}
         </mesh>
     </group>
   )
